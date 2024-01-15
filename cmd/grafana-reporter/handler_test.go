4081295617383ddb4bede0994f597283f108e040
Arkadii <42435266+platofff@users.noreply.github.com>
2024-01-15 10:19:15 +0300

    Update handler_test.go

--- cmd/grafana-reporter/handler_test.go
+++ cmd/grafana-reporter/handler_test.go
@@ -25,8 +25,8 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/platofff/reporter/grafana"
+	"github.com/platofff/reporter/report"
 	"github.com/gorilla/mux"
 	. "github.com/smartystreets/goconvey/convey"
 )
